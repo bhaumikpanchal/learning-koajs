2025d75a3fc4696458b27e1ec7b096a2a6353ab9
Bhaumik Panchal <bhaumik89@gmail.com>
2016-06-23 14:16:57 -0400

    exercise 2 - routing

--- program.js
+++ program.js
@@ -1,9 +1,28 @@
 var koa = require('koa');
 var app = koa();
 
-var port = process.argv[2];
-app.listen(port);
+app.use(function* (next) {
+  if(this.path !== "/") {
+    return yield next;
+  }
 
-app.use(function *() {
   this.body = 'hello koa';
 });
+
+app.use(function* (next) {
+  if(this.path !== "/404") {
+    return yield next;
+  }
+
+  this.body = 'page not found';
+});
+
+app.use(function* (next) {
+  if(this.path !== "/500") {
+    return yield next;
+  }
+
+  this.body = 'internal server error';
+});
+
+app.listen(process.argv[2]);
